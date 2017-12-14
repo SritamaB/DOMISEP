a1e85887c8ebf056e87a17a2c968d516756edbf4
DOMISEP_G4 <domisep_g4@github.com>
2017-12-14 14:39:16 +0100

    fist commit

---
+++ dashboard (1).php
@@ -0,0 +1,149 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Smart Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://webthemez.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/register.css" rel="stylesheet" />
+</head>
+<body>
+<div id="wrapper">
+		<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><p></P></a>
+					<a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+						
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header><!-- end header -->
+
+
+<form action="profile.php">
+  <div class="container">
+  <div class="col-md-offset-3 col-md-6">  
+      <button type="submit" class="btn btn-success btn-md col-md-3 pull-left">profile</button>
+    </div>
+	  </div>
+  </div>
+</form>
+<form action="#">
+  <div class="container">
+  <div class="col-md-offset-3 col-md-6">  
+      <button type="submit" class="btn btn-success btn-md col-md-3 pull-left">Set Up Apartment</button>
+    </div>
+	  </div>
+  </div>
+</form>
+<form action="#">
+  <div class="container">
+  <div class="col-md-offset-3 col-md-6">  
+      <button type="submit" class="btn btn-success btn-md col-md-3 pull-left">View Apartment</button>
+    </div>
+	  </div>
+  </div>
+</form>
+<form action="#">
+  <div class="container">
+  <div class="col-md-offset-3 col-md-6">  
+      <button type="submit" class="btn btn-success btn-md col-md-3 pull-left">Manage Home</button>
+    </div>
+	  </div>
+  </div>
+</form>
+ 	
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="widget">
+				<center>
+					<h5 class="widgetheading">Our Contact</h5>
+					<address>
+					<strong>opp:ID HOSPITAL,</strong><br>
+					Amaravathi Road,<br>
+					 Guntur,ANDHRA PRADESH</address>
+					<p>
+						<i class="icon-phone"></i>0863-2323239<br>
+						<i class="icon-envelope-alt"></i>manojgrandhotel@gmail.com
+					</p>
+					</center>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<!--<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="#">Latest Events</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Latest posts</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Recent News</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; smart home@2017 All right reserved.</span><a href="" target="_blank"></a>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer></html>
---
+++ js (1)/login (1).php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Smart Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://webthemez.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/register.css" rel="stylesheet" />
+ 
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+		<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><p></P></a>
+					<a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+					<li><a href="index.html">Home</a></li>
+						<li class="active"><a href="login.php">Login</a></li>
+						<li><a href="signup.php">Sign-Up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header><!-- end header -->
+
+
+<form action="#">
+  <div class="container">
+  <div class="col-md-offset-3 col-md-6">
+  <h2>Login</h2>
+  <hr />
+  <p><b>User Name:</b></p>
+    <input type="text"  name="name" required>
+	<p><b>Password:</b></p>
+    <input type="password" name="pass" required>
+    &nbsp;<b><a href="forgot.html"><b>Forgot Password</b></a></p>
+	<br>
+	<br>
+	<br>
+    <div class="clearfix">
+      <p>If you don't have an account please register <a href="signup.php">here</a></p>
+      <button type="submit" class="btn btn-success btn-md col-md-3 pull-right">Login</button>
+    </div>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+  </div>
+  </div>
+</form>
+ 	
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="widget">
+				<center>
+					<h5 class="widgetheading">Our Contact</h5>
+					<address>
+					<strong>xxxxxxxxxxxxxxxxx,</strong><br>
+					xxxxxxxxxxxxxxxx,<br>
+					 xxxxxxxxxxxxxxxxxxxx</address>
+					<p>
+						<i class="icon-phone"></i>xxxxxxxxxxxx<br>
+						<i class="icon-envelope-alt"></i>xxxxxxxxxxxxxxxx@xxxxxxxx.xxx
+					</p>
+					</center>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<!--<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="#">Latest Events</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Latest posts</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Recent News</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; smart home@2017 All right reserved.</span><a href="" target="_blank"></a>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer></html>
