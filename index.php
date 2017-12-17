c81915fd0a8a1f5c966589beb7fac40181010be0
loogar <34547415+loogar@users.noreply.github.com>
2017-12-17 15:09:42 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,150 +1,332 @@
-<?php
-if(!empty($_POST["register-user"])) {
-	/* Form Required Field Validation */
-	foreach($_POST as $key=>$value) {
-		if(empty($_POST[$key])) {
-		$error_message = "All Fields are required";
-		break;
-		}
-	}
-	/* Password Matching Validation */
-	if($_POST['password'] != $_POST['confirm_password']){ 
-	$error_message = 'Passwords should be same<br>'; 
-	}
-
-	/* Email Validation */
-	if(!isset($error_message)) {
-		if (!filter_var($_POST["userEmail"], FILTER_VALIDATE_EMAIL)) {
-		$error_message = "Invalid Email Address";
-		}
-	}
-
-	/* Validation to check if gender is selected */
-	if(!isset($error_message)) {
-	if(!isset($_POST["gender"])) {
-	$error_message = " All Fields are required";
-	}
-	}
-
-	/* Validation to check if Terms and Conditions are accepted */
-	if(!isset($error_message)) {
-		if(!isset($_POST["terms"])) {
-		$error_message = "Accept Terms and Conditions to Register";
-		}
-	}
-
-	if(!isset($error_message)) {
-		require_once("dbcontroller.php");
-		$db_handle = new DBController();
-		$query = "INSERT INTO registered_users (user_name, first_name, last_name, password, email, gender) VALUES
-		('" . $_POST["userName"] . "', '" . $_POST["firstName"] . "', '" . $_POST["lastName"] . "', '" . md5($_POST["password"]) . "', '" . $_POST["userEmail"] . "', '" . $_POST["gender"] . "')";
-		$result = $db_handle->insertQuery($query);
-		if(!empty($result)) {
-			$error_message = "";
-			$success_message = "You have registered successfully!";	
-			unset($_POST);
-		} else {
-			$error_message = "Problem in registration. Try Again!";	
-		}
-	}
-}
-?>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title>PHP User Registration Form</title>
-<style>
-body{
-	width:610px;
-	font-family:calibri;
-}
-.error-message {
-	padding: 7px 10px;
-	background: #fff1f2;
-	border: #ffd5da 1px solid;
-	color: #d6001c;
-	border-radius: 4px;
-}
-.success-message {
-	padding: 7px 10px;
-	background: #cae0c4;
-	border: #c3d0b5 1px solid;
-	color: #027506;
-	border-radius: 4px;
-}
-.demo-table {
-	background: #d9eeff;
-	width: 100%;
-	border-spacing: initial;
-	margin: 2px 0px;
-	word-break: break-word;
-	table-layout: auto;
-	line-height: 1.8em;
-	color: #333;
-	border-radius: 4px;
-	padding: 20px 40px;
-}
-.demo-table td {
-	padding: 15px 0px;
-}
-.demoInputBox {
-	padding: 10px 30px;
-	border: #a9a9a9 1px solid;
-	border-radius: 4px;
-}
-.btnRegister {
-	padding: 10px 30px;
-	background-color: #3367b2;
-	border: 0;
-	color: #FFF;
-	cursor: pointer;
-	border-radius: 4px;
-	margin-left: 10px;
-}
-</style>
+<meta charset="utf-8">
+<title>sweet Home</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://webthemez.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet" />
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
 </head>
 <body>
-<form name="frmRegistration" method="post" action="">
-<table border="0" width="500" align="center" class="demo-table">
-<?php if(!empty($success_message)) { ?>	
-<div class="success-message"><?php if(isset($success_message)) echo $success_message; ?></div>
-<?php } ?>
-<?php if(!empty($error_message)) { ?>	
-<div class="error-message"><?php if(isset($error_message)) echo $error_message; ?></div>
-<?php } ?>
-<tr>
-<td>User Name</td>
-<td><input type="text" class="demoInputBox" name="userName" value="<?php if(isset($_POST['userName'])) echo $_POST['userName']; ?>"></td>
-</tr>
-<tr>
-<td>First Name</td>
-<td><input type="text" class="demoInputBox" name="firstName" value="<?php if(isset($_POST['firstName'])) echo $_POST['firstName']; ?>"></td>
-</tr>
-<tr>
-<td>Last Name</td>
-<td><input type="text" class="demoInputBox" name="lastName" value="<?php if(isset($_POST['lastName'])) echo $_POST['lastName']; ?>"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" class="demoInputBox" name="password" value=""></td>
-</tr>
-<tr>
-<td>Confirm Password</td>
-<td><input type="password" class="demoInputBox" name="confirm_password" value=""></td>
-</tr>
-<tr>
-<td>Email</td>
-<td><input type="text" class="demoInputBox" name="userEmail" value="<?php if(isset($_POST['userEmail'])) echo $_POST['userEmail']; ?>"></td>
-</tr>
-<tr>
-<td>Gender</td>
-<td><input type="radio" name="gender" value="Male" <?php if(isset($_POST['gender']) && $_POST['gender']=="Male") { ?>checked<?php  } ?>> Male
-<input type="radio" name="gender" value="Female" <?php if(isset($_POST['gender']) && $_POST['gender']=="Female") { ?>checked<?php  } ?>> Female
-</td>
-</tr>
-<tr>
-<td colspan=2>
-<input type="checkbox" name="terms"> I accept Terms and Conditions <input type="submit" name="register-user" value="Register" class="btnRegister"></td>
-</tr>
-</table>
-</form>
-</body></html>
+<div id="wrapper">
+	<!-- start header -->
+	<header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><p></P></a>
+					<a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="logo"/></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+					    <li clas="active"><a href="index.php">Home</a></li>
+						<li><a href="login.php">Login</a></li>
+						<li><a href="signup.php">Sign-Up</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header>
+	<!-- end header -->
+	<section id="featured">
+
+	<!-- Slider -->
+        <div id="main-slider" class="flexslider">
+
+		<div class="flex-caption">
+
+                </div>
+            <ul class="slides">
+
+              <li>
+                <img src="img/slides/1.jpg" alt="" />
+
+              </li>
+              <li>
+                <img src="img/slides/2.jpg" alt="" />
+
+              </li>
+              <li>
+                <img src="img/slides/3.jpg" alt="" />
+				</li>
+				<li>
+                <img src="img/slides/4.jpg" alt="" />
+				</li>
+				<li>
+                <img src="img/slides/6.jpeg" alt="" />
+
+              </li>
+            </ul>
+        </div>
+	<!-- end slider -->
+
+	</section>
+	<ul class="slides">
+	  <img src="img/slides/5.jpg" alt="" />
+  </ul>
+  <!--<section portifi="search-form">
+      <div class="container">
+  <div class="row">
+  <div class="col-md-12 col-sm-12">
+  <div class="quick-search">-->
+  <section class="container tm-home-section-1" id="more">
+		<div class="row">
+			
+         
+	<section id="inner-headline">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+			<center>
+				<h2 class="pageTitle">GET IN TOUCH</h2>
+			</div>
+		</div>
+		</center>
+	</div>
+	</section>
+	<section id="content">
+	
+	<div class="container">
+	<div class="row">
+								<div class="col-md-6">
+								<div class="alert alert-success hidden" id="contactSuccess">
+										<strong>Success!</strong> Your message has been sent to us.
+									</div>
+									<div class="alert alert-error hidden" id="contactError">
+										<strong>Error!</strong> There was an error sending your message.
+									</div>
+									<div class="contact-form">
+										  <form method="POST" id="contact-form" class="form-horizontal" action="contactengine.php" onSubmit="alert('Thank you for your feedback!');">
+							<div class="col-sm-12">
+							<div class="form-group">
+							<input type="text" name="Name" id="Name" class="form-control wow fadeInUp" placeholder="Name" required/>
+							</div>
+							<div class="form-group">
+							<input type="text" name="Email" id="Email" class="form-control wow fadeInUp" placeholder="Email" required/>
+							</div>					
+							<div class="form-group">
+							<textarea name="Message" rows="10" cols="20" id="Message" class="form-control input-message wow fadeInUp"  placeholder="Message" required></textarea>
+							</div>
+							<div class="form-group">
+							<input type="submit" name="submit" value="Submit" class="btn btn-success wow fadeInUp" />
+							</div>
+							</div>
+						</form>		
+									</div>	</section>
+	<!--<div class="testimonial-area">
+    <div class="testimonial-solid">
+        <div class="container">
+            <div class="testi-icon-area">
+                <div class="quote">
+                    <i class="fa fa-microphone"></i>
+                </div>
+            </div>-->
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                      </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+	<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="widget">
+				<center>
+					<h5 class="widgetheading">Our Contact</h5>
+					<address>
+					<strong>opp:ID HOSPITAL,</strong><br>
+					Amaravathi Road,<br>
+					 Guntur,ANDHRA PRADESH</address>
+					<p>
+						<i class="icon-phone"></i>0863-2323239<br>
+						<i class="icon-envelope-alt"></i>manojgrandhotel@gmail.com
+					</p>
+					</center>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<!--<div class="widget">
+					<h5 class="widgetheading">Quick Links</h5>
+					<ul class="link-list">
+						<li><a href="#">Latest Events</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Privacy policy</a></li>
+						<li><a href="#">Career</a></li>
+						<li><a href="#">Contact us</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Latest posts</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="widget">
+					<!--<h5 class="widgetheading">Recent News</h5>-->
+					<!--<ul class="link-list">
+						<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+						<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+						<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+					</ul>-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; smart home@2017 All right reserved.</span><a href="" target="_blank"></a>
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      		<!-- jQuery -->
+  	
+<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/gallery/jquery.quicksand.js"></script>
+<script src="js/gallery/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/owl-carousel/owl.carousel.js"></script>
+<script>
+		// HTML document is loaded. DOM is ready.
+		$(function() {
+
+			$('#hotelCarTabs a').click(function (e) {
+			  e.preventDefault()
+			  $(this).tab('show')
+			})
+
+        	$('.date').datetimepicker({
+            	format: 'MM/DD/YYYY'
+            });
+            $('.date-time').datetimepicker();
+
+			// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+		  	$('a[href*=#]:not([href=#])').click(function() {
+			    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			      var target = $(this.hash);
+			      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			      if (target.length) {
+			        $('html,body').animate({
+			          scrollTop: target.offset().top
+			        }, 1000);
+			        return false;
+			      }
+			    }
+		  	});
+		});
+		
+		// Load Flexslider when everything is loaded.
+		$(window).load(function() {	  		
+			// Vimeo API nonsense
+
+/*
+			  var player = document.getElementById('player_1');
+			  $f(player).addEvent('ready', ready);
+			 
+			  function addEvent(element, eventName, callback) {
+			    if (element.addEventListener) {
+			      element.addEventListener(eventName, callback, false)
+			    } else {
+			      element.attachEvent(eventName, callback, false);
+			    }
+			  }
+			 
+			  function ready(player_id) {
+			    var froogaloop = $f(player_id);
+			    froogaloop.addEvent('play', function(data) {
+			      $('.flexslider').flexslider("pause");
+			    });
+			    froogaloop.addEvent('pause', function(data) {
+			      $('.flexslider').flexslider("play");
+			    });
+			  }
+*/
+
+			 
+			 
+			  // Call fitVid before FlexSlider initializes, so the proper initial height can be retrieved.
+/*
+
+			  $(".flexslider")
+			    .fitVids()
+			    .flexslider({
+			      animation: "slide",
+			      useCSS: false,
+			      animationLoop: false,
+			      smoothHeight: true,
+			      controlNav: false,
+			      before: function(slider){
+			        $f(player).api('pause');
+			      }
+			  });
+*/
+
+
+			  
+
+//	For images only
+		    $('.flexslider').flexslider({
+			    controlNav: false
+		    });
+
+
+	  	});
+	</script>
+</body>
+</html>
+
